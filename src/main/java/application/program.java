3ac03e85ba78f51693679ab8f5eb75c3e92d6d1d
danieloliviak <danieloliviak355@gmail.com>
2025-03-16 16:49:28 +0000

    First class: position

--- src/main/java/application/program.java
+++ src/main/java/application/program.java
@@ -1,7 +1,10 @@
 package application;
 
+import boardGame.Position;
+
 public class program {
     public static void main(String[] args) {
-
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }
 }
